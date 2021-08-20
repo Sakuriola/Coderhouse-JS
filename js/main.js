6f61e914f6c04883d63839d47133df6533b750ff
Fabiorlan Dyan <yhoyannaasakura1992@gmail.com>
2021-08-19 21:58:04 -0400

    Desafio número 10 (clase 14) en JS de Coderhouse

--- js/main.js
+++ js/main.js
@@ -34,4 +34,38 @@ $(document).ready(function () {
 	hideAwayThree = () => {
 		$('#div1').fadeOut(3000)
 	}
+
+	$('#chained-animation').click(() => {
+		$('#div1').fadeIn(1000)
+		$('#div2').fadeIn(2000)
+		$('#div3').fadeIn(3000)
+		$('#div4').fadeIn(hideAway())
+	})
+
+	$('#hp_api').click(() => {
+		var hp = 'https://hp-api.herokuapp.com/api'
+		$.ajax({
+			url: `${hp}/characters`,
+			method: 'GET',
+		}).then((response) => {
+			console.log(response)
+			response.forEach((item) => {
+				$('#hp_output').append(`
+				<div class='api-global-container'>
+					<div class='api-main-container'>
+						<div class='api-image-container'>
+							<img class='api-image' src="${item.image}">
+						</div>
+						
+						<h3 class='api-name'>Nombre: ${item.name}</h3>
+						<h3 class='api-house'>Casa: ${item.house ? item.house : 'No especificado'}</h3>
+						<h3 class='api-gender'>Fecha de nacimiento: ${
+							item.dateOfBirth ? item.dateOfBirth : 'No especificado'
+						}</h3>
+					</div>
+				</div>
+				`)
+			})
+		})
+	})
 })
